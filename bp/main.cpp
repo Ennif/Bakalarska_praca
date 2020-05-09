5c65b1360433083e71f29474c54d014ef15fd3bd
japubek <jakubtrstensky@gmail.com>
2020-05-09 19:55:12 +0200

    test commit

--- bp/main.cpp
+++ bp/main.cpp
@@ -9,7 +9,7 @@ int main() {
 
     db_connection *dbConnection = new db_connection();
     //dbConnection->connectToDatabase();
-    cout << "ahoja";
+    cout << "ahoj";
     delete dbConnection;
     }
 
