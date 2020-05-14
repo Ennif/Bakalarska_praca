8c2fc773e5a234a0fbe51ea8f44fcadeaecf8a06
japubek <jakubtrstensky@gmail.com>
2020-05-14 21:28:14 +0200

    implemented a program cycle alpha

--- bp/main.cpp
+++ bp/main.cpp
@@ -1,24 +1,60 @@
-#include "Database/database_local/db_localDatabase.h"
 
 #include <iostream>
-
-int selectSenzorData();
+#include <vector>
+#include <chrono>
+#include <string>
+#include "CSV/csv_handler.h"
+#include "data/Generated/generated_data.h"
 
 using namespace std;
 
+//declarations
+int getRandValue();
+time_t getTimeStamp();
+
+void read_csv(string csv_filename);
+vector<string> split(string,char);
+
+
 int main() {
+    vector<vector<int>> firstSensor;
+    vector<vector<int>> secondSensor;
+    int counter = 0;
 
-   /* db_connection *dbConnection = new db_connection();
-    dbConnection->connectToDatabase();
-    delete dbConnection;*/
+    vector<vector<int>> data;
 
-   db_localDatabase *localDatabaseHandler = new db_localDatabase();
-   //localDatabaseHandler->insertSenzorsData(); //  pair<int,float>
-   localDatabaseHandler->selectSenzorData();
-   localDatabaseHandler->deleteSenzorData();
-   delete localDatabaseHandler;
+    csv_handler *csvHandler = new csv_handler();
+    csvHandler->createCSVFile();
 
+    generated_data *generatedData = new generated_data();
+
+   chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    while(1) {
+        if(counter == 50){
+            break;
+        }
+        if(counter % 10 == 0){
+            vector<int> temp;
+            temp.push_back(generatedData->getRandomSensorData());
+            temp.push_back(generatedData->getTimestamp());
+            secondSensor.push_back(temp);
+        }
+        vector<int> temp;
+        temp.push_back(generatedData->getRandomSensorData());
+        temp.push_back(generatedData->getTimestamp());
+        firstSensor.push_back(temp);
+        counter++;
+    }
+
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << " [ms]" << endl;
+
+    return 0;
 }
+
+
+
+
 
 
 
