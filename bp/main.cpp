c45f1ac82720378945e1f7e3c40a7e39dfb9c0b6
japubek <jakubtrstensky@gmail.com>
2020-05-19 02:44:17 +0200

    build commit

--- bp/main.cpp
+++ bp/main.cpp
@@ -1,12 +1,11 @@
 #include "APP/app_handler.h"
 
 int main() {
-    /*chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    auto *appHandler = new app_handler();
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    appHandler->mainProgram();
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
-    cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << " [ms]" << endl;*/
-
-   auto *appHandler = new app_handler();
-   appHandler->mainProgram();
+    cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << " [ms]" << endl;
    return 0;
 }
 
